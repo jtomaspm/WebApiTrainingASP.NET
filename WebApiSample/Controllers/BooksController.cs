6bd376e569a2f01d6002e7ba1f4601fb0b71645f
jtoma <jtoma@DESKTOP-TQMCH89>
2022-07-17 12:27:12 +0100

    Add project files.

---
+++ WebApiSample/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private String[]? content = Array.Empty<String>();
+
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            if (content.Length > 0) {
+                return content;
+            }
+            return new[] { "test", "book" };
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            content = new[] { "test", "changed" };
+        }
+
+    }
+}
